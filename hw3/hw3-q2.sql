d2af48ef8c50b9f380b52f29b307176e3af4b57a
İsmail Sefa Esgün <isefaesgun@gmail.com>
2023-09-20 18:45:52 +0300

    hw3 added

---
+++ hw3/hw3-q2.sql
@@ -0,0 +1,3 @@
+SELECT country
+FROM country
+WHERE country LIKE '%n' AND length(country) >= 6;
---
+++ hw3/hw3-q3.sql
@@ -0,0 +1,4 @@
+SELECT title
+FROM film
+WHERE title ILIKE '%t%t%t%t%';
+-- En az 4 buyuk veya kucuk harf T içeren film isimleri
