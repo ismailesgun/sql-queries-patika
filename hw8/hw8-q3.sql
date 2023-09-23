38ddf5d695540034f6ad02d72c46766bba6eb149
İsmail Sefa Esgün <isefaesgun@gmail.com>
2023-09-23 14:16:34 +0300

    hw8 added

---
+++ hw8/hw8-q3.sql
@@ -0,0 +1,27 @@
+UPDATE employee
+SET name = 'Deniz',
+    email = 'ocean123@gmail.com',
+	birthday = '1970-01-03'
+WHERE id = 7;
+	
+UPDATE employee
+SET email = 'fanya94@hotmail.com',
+	birthday = '1984-12-06'
+WHERE name = 'Fanya';
+
+UPDATE employee
+SET email = '',
+	name = 'Mary'
+WHERE birthday = '2022-10-09';
+
+UPDATE employee
+SET birthday = '1990-02-20',
+	name = 'John'
+WHERE email = 'osmewin1@liveinternet.ru';
+
+UPDATE employee
+SET birthday = '01-01-1966',
+	name = 'Gaye',
+	email = 'gaye_66@gmail.com'
+WHERE id = 5;
+
---
+++ hw8/hw8-q4.sql
@@ -0,0 +1,15 @@
+DELETE FROM employee
+WHERE id = 7;
+	
+DELETE FROM employee
+WHERE name = 'Fanya';
+
+DELETE FROM employee
+WHERE birthday = '2022-10-09';
+
+DELETE FROM employee
+WHERE email = 'osmewin1@liveinternet.ru';
+
+DELETE FROM employee
+WHERE id = 5;
+
